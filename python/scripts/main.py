643dfb26a06d61a7d52fcdd9df0c6573e656bcfc
Václav Jež <jez.vasek@gmail.com>
2025-12-02 17:04:01 +0100

    reverted to before dynamic timeframe and rolling agregation features

--- python/scripts/main.py
+++ python/scripts/main.py
@@ -9,54 +9,46 @@ from python.scripts.services.walk_forward import walk_forward_test
 from python.scripts.services.evaluation import evaluate_class, evaluate_regg
 
 
-# TODO
-# mozna GD LR, XGBoost, LightGBM - overfitting?
-# regularizace L1 / L2 u linear models
-
-
 
 df = pd.read_csv('python/cleandata/processed_data.csv')
 
+# log return
+df["log_return"] = np.log(df['adjusted'] / df['adjusted'].shift(1))
+df.dropna(inplace=True)
 
-# TIMEFRAME PRO VYPOCET LOG RETURN
-TIMEFRAME = 5
-"""
- = 1 → denní return
- = 5 → týdenní return
- = 20 → měsíční return
- = 60 → kvartální return
-"""
+
+# DYNAMICKY PARAMETR TIMEFRAME
+# TODO
+# nastavit parametr při volání timeframe
+# predikce ukládat s označením timeframe
+# vytvářet rolling jen pri timeframe > 1 shift u log return taky
+
+# AGREGACE FEATURES NA 5 DENNI TIMEFRAME - rolling
+# TODO
+# https://chatgpt.com/share/692de068-8030-8000-83ae-cba5550092d3
+
+# regularizace L1 / L2 u linear models
+# mensi learning rate, max depth u tree models - hlavne XGBOOST, LightGBM
+# TODO
+
+
 
 features = [
     # Unused
     # sma_20, sma_50, stochrsi, macd_signal, bb_up, bb_dn, g_trends
 
-    # Log return vstupuje jen v lag -> autoregresni feature
-    'log_return',
-    'vix',
-    'bull_bear_spread_surv',
-    'volume',
-    'ema_20',
-    'basic_volatility', 'atr','macd', 'obv', 'rsi', 'adx',
-    'sentiment_neutral', 'sentiment_positive', 'sentiment_negative', 'sentiment_none',
+    'log_return', 'vix', 'sentiment_neutral', 'sentiment_positive', 'sentiment_negative', 'sentiment_none',
+    'bull_bear_spread_surv', 'volume', 'ema_20', 'basic_volatility', 'atr', 'macd',
+    'obv', 'rsi', 'adx'
 ]
+
+
 features_lin_models = features.copy()
-features_lin_models.remove('sentiment_none')
+features_lin_models.remove("sentiment_none")
 
-# log return
-df["log_return"] = np.log(df['adjusted'] / df['adjusted'].shift(TIMEFRAME))
-df.dropna(inplace=True)
-
-
-# def timeframe_agg(all_features, df):
-#     if TIMEFRAME == 1:
-#         return df
-#
-#     df_out = df[list(all_features.keys())].rolling(window=TIMEFRAME).agg(all_features).dropna()
-#     return df_out
 
 # vytvoreni umele sekvence lagu -> kazdy radek obsahuje Pocet f * Pocet lags
-def create_lags(lags, all_features, df):
+def create_lags(lags, all_features):
     X = pd.DataFrame()
 
     for l in range(1, lags + 1):
@@ -68,17 +60,13 @@ def create_lags(lags, all_features, df):
 
 lags = 10
 
-# df_final = timeframe_agg(features, df)
-
-X = create_lags(lags, features, df)
-# Vynechani referencni urovne pro kategorie sentiment feature pro linearni modely
-X_linear = create_lags(lags, features_lin_models, df)
-
+X = create_lags(lags, features)
+X_linear = create_lags(lags, features_lin_models)
 features_names = X.columns
-
 
 y = df["log_return"]
 y_clf = (y > 0.005).astype(int)
+
 # Omezeni radku dle hodnoty LAG
 y, y_clf = y[lags:], y_clf[lags:]
 
@@ -93,16 +81,15 @@ window = 2630
 
 
 # -----Main walk forward loop-----
-walk_forward_test(
-    TIMEFRAME,
-    X,
-    X_linear,
-    y,
-    y_clf,
-    window,
-    gd_learning_rate,
-    gd_epochs,
-)
+# walk_forward_test(
+#     X,
+#     X_linear,
+#     y,
+#     y_clf,
+#     window,
+#     gd_learning_rate,
+#     gd_epochs,
+# )
 
 
 # LOAD TRAINED MODELS AND PREDICTIONS:
